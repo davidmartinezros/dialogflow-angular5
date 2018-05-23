cdf91f184ebbeb3d40bb687296a62f3c0be4244e
David Martinez Ros <davidnezan@gmail.com>
2018-05-23 19:36:43 +0200

    make the event of enter and placeholder and make the web responsive in differents devices

--- src/app/components/message-form/message-form.component.ts
+++ src/app/components/message-form/message-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, AfterContentInit, ContentChild,AfterViewInit, ViewChild, ViewChildren } from '@angular/core';
 import { Message } from '../../models';
 import { DialogflowService } from '../../services';
 
@@ -8,6 +8,8 @@ import { DialogflowService } from '../../services';
   styleUrls: ['./message-form.component.scss']
 })
 export class MessageFormComponent implements OnInit {
+
+  @ViewChildren('inputText') inpuText;
 
   @Input('message')
   public message : Message;
@@ -20,17 +22,32 @@ export class MessageFormComponent implements OnInit {
   ngOnInit() {
   }
 
-  public sendMessage(): void {
-    this.message.timestamp = new Date();
-    this.messages.push(this.message);
-
-    this.dialogFlowService.getResponse(this.message.content).subscribe(res => {
-      this.messages.push(
-        new Message(res.result.fulfillment.speech, 'assets/images/bot.png', res.timestamp)
-      );
-    });
-
-    this.message = new Message('', 'assets/images/user.png');
+  ngAfterViewInit() {            
+    this.inpuText.first.nativeElement.focus();
   }
 
+  public sendMessage(): void {
+    if(this.message.content) {
+      this.message.timestamp = new Date();
+      this.messages.push(this.message);
+
+      this.dialogFlowService.getResponse(this.message.content).subscribe(res => {
+        this.messages.push(
+          new Message(res.result.fulfillment.speech, 'assets/images/bot.png', res.timestamp)
+        );
+      });
+
+      this.message = new Message('', 'assets/images/user.png');
+    }
+    this.inpuText.first.nativeElement.focus();
+  }
+
+  public keyPressFunction(event) {
+    //console.log(event);
+    // si premem la tecla return
+    if(event.keyCode == 13) {
+      this.sendMessage();
+    }
+  }
+ 
 }
